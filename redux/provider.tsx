9bdfc56a4d2f1d68733aa1a5bfde45a33670b107
Emeey-Lanr <emeeylanr04@gmail.com>
2024-03-06 13:37:59 +0100

    added some packages and and did the folder structuring

---
+++ redux/provider.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store'
+const AppProvider = ({children}:{children:React.ReactNode}) => {
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+
+  )
+}
+export default AppProvider
